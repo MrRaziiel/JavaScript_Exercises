e45ed8043006c4c89adb6691d4c470d55b0e43ba
MrRaziiel <116804148+MrRaziiel@users.noreply.github.com>
2022-12-06 17:16:40 +0000

    Delete Exercises/w3resource exercise/print contents current window directory

--- Exercises/w3resource exercise/print contents current window/areOfTriangle/index.js
+++
@@ -1,9 +0,0 @@
-var firstSide = 7;
-var secondSide = 6;
-var thirdsSide = 5;
-
-var s = (firstSide + secondSide + thirdsSide)/2;
-console.log(s);
-var areaOfTraingle = Math.sqrt(s*((s-firstSide)*(s-secondSide)*(s-thirdsSide)));
-
-console.log(areaOfTraingle);
